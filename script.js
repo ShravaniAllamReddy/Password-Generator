209b4e94145490486521df122dc5d515c779bc57
Shravani Allam Reddy <shravani.allam@gmail.com>
2020-08-26 16:05:21 -0600

    added code to generate random password and checked for length condition

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // Constants
 const generateBtn = document.querySelector("#generate");
+const pwd = {
+
+  lowerCase: 'abcdefghijklmnopqrstuvwxyz',
+
+  upperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+
+  numeric: '0123456789',
+
+  specialCharacters: '!"#$%&\'()*+,-./:;<=>?@[]^_{|}~'
+
+};
 
 // Functions
 
@@ -9,24 +20,34 @@ const generateBtn = document.querySelector("#generate");
 function writePassword() {
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
 /**
  * generate a password based on certain criteria
  */
-function generatePassword(){
+function generatePassword() {
 
   // Password is currently blank! We need to make a better one
   let password = "";
 
+  let pwdLength = prompt("How many characters would you like your password to contain?");
+  // let characterType = prompt(" what type of characters you want to include");
+  // let confirmSpecial = confirm("Click OK to include specialCharacters");
+  if (pwdLength < "8" && pwdLength > "123") {
+    pwdLength = prompt("Choose the password length between 8 to 123");
+  }
 
+  let userChoices = pwd.lowerCase + pwd.upperCase + pwd.numeric + pwd.specialCharacters;
 
+  for (let i = 0; i < pwdLength; i++) {
 
+    password += userChoices[Math.floor(Math.random() * userChoices.length)];
 
+  };
   return password;
 }
+
 
 // Main Process
 
